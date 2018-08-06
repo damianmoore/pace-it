75d41adab11f043f927db408709475a2e1707883
Damian Moore <damian@epixstudios.co.uk>
2018-08-06 08:19:37 +0100

    Basic distance tracking

--- app/src/main/java/uk/co/epixstudios/pace_it/paceit/TrackActivity.java
+++ app/src/main/java/uk/co/epixstudios/pace_it/paceit/TrackActivity.java
@@ -11,17 +11,27 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.content.ContentValues.TAG;
+import java.util.ArrayList;
+import java.util.List;
 
+import static android.content.ContentValues.TAG;
 
 public class TrackActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "uk.co.epixstudios.pace_it.TRACK_ACTIVITY";
 
     TextView gps_position;
-    TextView distance;
+    TextView distance_text;
+//    ArrayList<Location> locations = new ArrayList<Location>();
+    Location prev_loc = null;
+    float distance = 0;
+    float total_distance = 0;
+    Button button_start_stop;
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,15 @@ public class TrackActivity extends AppCompatActivity {
         setContentView(R.layout.activity_track);
 
         gps_position = (TextView) findViewById(R.id.gps_position);
-        distance = (TextView) findViewById(R.id.distance);
+        distance_text = (TextView) findViewById(R.id.distance);
+        button_start_stop = (Button) findViewById(R.id.button_start_stop);
+
+        if (!running) {
+            button_start_stop.setText("Start");
+        }
+        else {
+            button_start_stop.setText("Stop");
+        }
 
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -43,21 +61,40 @@ public class TrackActivity extends AppCompatActivity {
         }
     }
 
+    public void onStartStop(View view) {
+        if (!running) {
+            running = true;
+            total_distance = 0;
+            button_start_stop.setText("Stop");
+            distance_text.setText("" + total_distance);
+        }
+        else {
+            running = false;
+            button_start_stop.setText("Start");
+            distance_text.setText("" + total_distance);
+        }
+    }
+
     private class MyLocationListener implements LocationListener {
 
         @Override
         public void onLocationChanged(Location loc) {
-            TextView editLocation = findViewById(R.id.gps_position);
-            editLocation.setText("");
-            Toast.makeText(
-                    getApplicationContext(),
-                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
-                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-            String longitude = "Longitude: " + loc.getLongitude();
-            Log.v(TAG, longitude);
-            String latitude = "Latitude: " + loc.getLatitude();
-            Log.v(TAG, latitude);
             gps_position.setText("Lat: " + loc.getLatitude() + "\nLng: " + loc.getLongitude());
+
+            if (prev_loc != null){
+
+                distance = prev_loc.distanceTo(loc);
+//                Toast.makeText(getApplicationContext(), " " + distance, Toast.LENGTH_SHORT).show();
+                if (running) {
+                    total_distance += distance;
+                }
+
+                Log.v(TAG, "" + total_distance);
+                distance_text.setText("" + total_distance);
+
+            }
+
+            prev_loc = loc;
         }
 
         @Override
